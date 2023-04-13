29db9a6b796f31ab0f3355323c0ddebab8bac57a
Saroar Hossain <101619899+xs-saroar@users.noreply.github.com>
2023-04-13 17:52:43 +0600

    Added: Home, Notion and Createpost page

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { WithContext as ReactTags } from "react-tag-input";
+
+
+const CreatePost = () => {
+    const navigate = useNavigate();
+
+    const [postTitle, setPostTitle] = useState("");
+    const [postContent, setPostContent] = useState("");
+
+    //...gets the publish date for the post
+    const currentDate = () => {
+        const d = new Date();
+        return `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+    };
+
+    //...logs the post details to the console
+    const addPost = (e) => {
+        e.preventDefault();
+        console.log({
+            postTitle,
+            postContent,
+            username: localStorage.getItem("username"),
+            timestamp: currentDate(),
+        });
+        navigate("/dashboard");
+    };
+
+    return (
+        <>
+            <div className='createPost'>
+                <h2>Create a new Post</h2>
+                <form className='createForm' onSubmit={addPost}>
+                    <label htmlFor='title'> Title</label>
+                    <input
+                        type='text'
+                        required
+                        value={postTitle}
+                        onChange={(e) => setPostTitle(e.target.value)}
+                        className='createForm__title'
+                    />
+
+                    <label htmlFor='title'> Content</label>
+                    <textarea
+                        required
+                        rows={15}
+                        value={postContent}
+                        onChange={(e) => setPostContent(e.target.value)}
+                        className='createForm__content'
+                    />
+
+                    <button className='createForm__button'>ADD POST</button>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default CreatePost;
