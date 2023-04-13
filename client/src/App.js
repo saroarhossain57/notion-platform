d5135dbcbd72f6570b006b7b24cc9b6b4d06872c
Saroar Hossain <101619899+xs-saroar@users.noreply.github.com>
2023-04-13 17:41:20 +0600

    Added: socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,7 @@
+import { io } from "socket.io-client";
+//👇🏻 http://localhost:4000 is where the server host URL.
+const socket = io.connect("http://localhost:4000");
+
 function App() {
   return (
       <div>
